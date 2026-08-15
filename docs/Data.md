f4228153b5fb4379a9a96c75b8c364a08cbe14f2
Oleksandr Kovalov <oleksandr.kovalov.work@gmail.com>
2026-08-16 02:10:31 +0300

    (Model) Feat: Regions

---
+++ docs/Data.md
@@ -0,0 +1,6 @@
+# Raw Data - Explanation
+
+### Regions
+
+Each region has a numerical code. You can find current codes in [region.rs](../model/src/region.rs). 
+Got them from [EDBO Registry](https://registry.edbo.gov.ua/vishcha-osvita) - there is interactive map.
---
+++ model/src/region.rs
@@ -0,0 +1,32 @@
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+
+/// Documentation - [link](../../docs/Data.md#Regions).
+#[derive(Debug, IntoPrimitive, TryFromPrimitive)]
+#[repr(i8)]
+pub enum Region {
+    KyivCity = 80,
+    Vinnytsia = 5,
+    Volyn = 7,
+    Dnipropetrovsk = 12,
+    Donetsk = 14,
+    Zhytomyr = 18,
+    Zakarpattia = 21,
+    Zaporizhzhia = 23,
+    IvanoFrankivsk = 26,
+    Kyiv = 32,
+    Kirovohrad = 35,
+    Luhansk = 44,
+    Lviv = 46,
+    Mykolaiv = 48,
+    Odesa = 51,
+    Poltava = 53,
+    Rivne = 56,
+    Sumy = 59,
+    Ternopil = 61,
+    Kharkiv = 63,
+    Kherson = 65,
+    Khmelnytskyi = 68,
+    Cherkasy = 71,
+    Chernivtsi = 73,
+    Chernihiv = 74,
+}
