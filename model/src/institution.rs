9f47a338deea6342f99665828be512de55f1e5d4
Oleksandr Kovalov <oleksandr.kovalov.work@gmail.com>
2026-08-16 23:37:47 +0300

    (Model) Feat: Institution

--- model/src/institution.rs
+++ model/src/institution.rs
@@ -1,1 +1,20 @@
+use crate::institution::category::InstitutionCategory;
+use crate::institution::ownership::OwnershipForm;
+use crate::region::Region;
+
 pub mod category;
+pub mod ownership;
+
+#[derive(Debug)]
+pub struct Institution {
+    pub title: String,
+    pub id: i16,
+    pub parent_id: Option<i16>,
+    pub short_name: Option<String>,
+    pub english_name: Option<String>,
+    pub is_from_crimea: bool,
+    pub registration_year: Option<i16>,
+    pub category: InstitutionCategory,
+    pub ownership_form: OwnershipForm,
+    pub region: Region,
+}
