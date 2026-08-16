2b764c9324fb44e478775a29c84b2e76ecc0c5e2
Oleksandr Kovalov <oleksandr.kovalov.work@gmail.com>
2026-08-16 23:37:40 +0300

    (Model) Feat: Institution ownership form

---
+++ model/src/institution/ownership.rs
@@ -0,0 +1,15 @@
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+use strum_macros::{Display, EnumString};
+
+#[derive(Debug, IntoPrimitive, TryFromPrimitive, EnumString, Display)]
+#[repr(i16)]
+pub enum OwnershipForm {
+    #[strum(serialize = "Державна")]
+    State = 1,
+    #[strum(serialize = "Комунальна")]
+    Municipal = 2,
+    #[strum(serialize = "Корпоративна")]
+    Corporate = 3,
+    #[strum(serialize = "Приватна")]
+    Private = 4,
+}
