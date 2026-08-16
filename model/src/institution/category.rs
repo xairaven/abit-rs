e106c86fdd735d097af64a7fabde9567c8cf60fe
Oleksandr Kovalov <oleksandr.kovalov.work@gmail.com>
2026-08-16 22:56:25 +0300

    (Model) Feat: Institute categories de/serializing

--- model/src/institution/category.rs
+++ model/src/institution/category.rs
@@ -1,15 +1,26 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use strum_macros::{Display, EnumString};
 
-#[derive(Debug, IntoPrimitive, TryFromPrimitive)]
+#[derive(Debug, IntoPrimitive, TryFromPrimitive, EnumString, Display)]
 #[repr(i16)]
 pub enum InstitutionCategory {
+    #[strum(serialize = "Заклад вищої освіти")]
     HigherEducation = 1,
+    #[strum(serialize = "Заклади фахової передвищої освіти")]
     ProfessionalCollege = 9,
+    #[strum(serialize = "Заклад професійної (професійно-технічної) освіти")]
     VocationalEducation = 2,
+    #[strum(serialize = "Заклад загальної середньої освіти")]
     SecondaryEducation = 3,
+    #[strum(serialize = "Наукові інститути (установи)")]
     ScientificInstitutes = 8,
+    #[strum(serialize = "Заклади післядипломної освіти")]
     Postgrad = 10,
 
+    #[strum(
+        serialize = "Інший заклад освіти, що надає професійну (професійно-технічну освіту)"
+    )]
     OtherVET = 4,
+    #[strum(serialize = "")]
     Unknown = 5,
 }
